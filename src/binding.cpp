148ab097224f202a010f611d7998d24a5afe724f
Niurx <754346443@qq.com>
2023-12-18 00:40:22 +0800

    12.18 tmp

---
+++ src/binding.cpp
@@ -0,0 +1,36 @@
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+void bind_Detection(py::module &m) {
+    m.def(
+        "Detection",
+        &Detection,
+        R"pbdoc(
+            Example function performing OLS.
+
+            Parameters
+            ----------
+            arr : np.array
+                input array
+
+            Returns
+            -------
+            coeffs: np.ndarray
+                coefficients
+            std_err : np.ndarray
+                standard error on the coefficients
+        )pbdoc"
+        // py::arg("TxAntNum"),
+        // py::arg("RxAntNum"),
+        // py::arg("SNRdB"),
+        // py::arg("ModType"),
+        // py::arg("sample")
+    );
+}
+
+PYBIND11_MODULE(mimo, m) {
+    bind_Detection(m);
+}
+
