ce818469eda2736fcaa905af8cd773d87ebc9424
Niurx <754346443@qq.com>
2023-12-18 00:17:06 +0800

    new start

---
+++ tests/test_basic.py
@@ -0,0 +1,10 @@
+import mimo as m
+
+
+def test_main():
+    assert m.__version__ == "0.0.1"
+    assert m.add(1, 2) == 3
+    assert m.subtract(1, 2) == -1
+
+if __name__ == "__main__":
+    test_main()
