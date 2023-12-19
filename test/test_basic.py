e45c5e356888fbf068ee9c9576447b27a59f50e8
Niurx <754346443@qq.com>
2023-12-20 02:23:51 +0800

    12.20 temp

--- test/test_basic.py
+++ test/test_basic.py
@@ -6,29 +6,38 @@ TxAntNum = 32
 RxAntNum = 64
 SNR = 10
 ModType = 4
-sample = 100000
-
-
-
-def run_detection():
-    errorBits, errorFrames = m.detection(TxAntNum, RxAntNum, ModType, SNR, sample)
-
-# Create a list to store the processes
-processes = []
+sample = 10000
 
 start = time.time()
-
-# Create and start a process for each iteration
-for _ in range(multiprocessing.cpu_count()):
-    process = multiprocessing.Process(target=run_detection)
-    process.start()
-    processes.append(process)
-
-# Wait for all processes to finish
-for process in processes:
-    process.join()
-
+errorBits, errorFrames = m.detection(TxAntNum, RxAntNum, ModType, SNR, sample)
 end = time.time()
 print("Time taken: ", end - start)
+
+a=1
+
+
+
+# def run_detection():
+#     errorBits, errorFrames = m.detection(TxAntNum, RxAntNum, ModType, SNR, sample)
+
+# if __name__ == "__main__":
+
+#     # Create a list to store the processes
+#     processes = []
+
+#     start = time.time()
+
+#     # Create and start a process for each iteration
+#     for _ in range(multiprocessing.cpu_count()):
+#         process = multiprocessing.Process(target=run_detection)
+#         process.start()
+#         processes.append(process)
+
+#     # Wait for all processes to finish
+#     for process in processes:
+#         process.join()
+
+#     end = time.time()
+#     print("Time taken: ", end - start)
  
  
