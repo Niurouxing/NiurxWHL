79a093646445d299bf5efe8f40c84762045335d1
Niurx <754346443@qq.com>
2023-12-21 14:30:37 +0800

    12.21 temp

--- include/detection.h
+++ include/detection.h
@@ -13,21 +13,29 @@ class Detection{
         static void createDetection(int TxAntNum, int RxAntNum, int ModType, double SNRdB);
         static Detection * getDetection();
 
-
+        // used in both real and complex domain
         int TxAntNum, RxAntNum, ModType, ConSize, bitLength;
         int TxAntNum2, RxAntNum2;
+
+        int * TxBits;
+        double SNRdB;
+        double Nv;
+        double sqrtNvDiv2;
+        double NvInv;
+
+
+        // used in real domain
         double * TxSymbols;
+        double ** H;
         double * RxSymbols;
         const double * Cons;
         const double * Cons2;
         const int * bitCons;
 
-        int * TxBits;
-        double ** H;
-        double SNRdB;
-        double Nv;
-        double sqrtNvDiv2;
-        double NvInv;
+
+
+
+
         void generateChannel();
         void generateTxSignals();
         void generateRxSignalsWithNoise();
