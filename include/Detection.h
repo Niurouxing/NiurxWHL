f4131ec0a4af0e08ef6f9589fbe6c20e617f590f
Niurx <754346443@qq.com>
2023-12-22 15:45:42 +0800

    12.22 temp2

---
+++ include/Detection.h
@@ -0,0 +1,68 @@
+#pragma once
+
+
+#include <complex>
+
+class Detection{
+    public:
+
+        Detection(int TxAntNum, int RxAntNum, int ModType, double SNRdB);
+
+        // used in both real and complex domain
+        int TxAntNum, RxAntNum, ModType, ConSize, bitLength;
+        int TxAntNum2, RxAntNum2;
+
+        int * TxBits;
+        double SNRdB;
+        double Nv;
+        double sqrtNvDiv2;
+        double NvInv;
+
+        virtual void generateChannel()=0;
+        virtual void generateTxSignals()=0;
+        virtual void generateRxSignalsWithNoise()=0;
+        virtual void generate();
+        virtual ~Detection()=default;
+};
+
+
+class DetectionRD : public Detection{
+    public:
+        DetectionRD(int TxAntNum, int RxAntNum, int ModType, double SNRdB);
+
+        // used in real domain
+        double * TxSymbols;
+        double ** H;
+        double * RxSymbols;
+        const double * Cons;
+        const double * Cons2;
+        const int * bitCons;
+
+        void generateChannel() override;
+        void generateTxSignals() override;
+        void generateRxSignalsWithNoise() override;
+
+        ~DetectionRD() override = default;
+};
+
+class DetectionCD : public Detection{
+    public:
+        DetectionCD(int TxAntNum, int RxAntNum, int ModType, double SNRdB);
+
+        // used in complex domain
+        std::complex<double> * TxSymbols;
+        std::complex<double> ** H;
+        std::complex<double> * RxSymbols;
+
+        const double * ConsReal;
+        const std::complex<double> * ConsComplex;
+
+        const int * bitConsReal;
+        const int * bitConsComplex;
+
+        void generateChannel() override;
+        void generateTxSignals() override;
+        void generateRxSignalsWithNoise() override;
+
+        ~DetectionCD() override = default;
+};
