8937f97b8ee6508f0e1fbf00c4066b7c11b2b556
Niurouxing <754346443@qq.com>
2023-12-25 04:37:00 +0800

    stop use python to iterate

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -1,62 +1,63 @@
 import os
-import multiprocessing
-from tqdm import tqdm
-import numpy as np
+# os.add_dll_directory("C:/Program Files/mingw64/bin")
+
 import mimo as m
+import multiprocessing
+import time
+from tqdm import tqdm
 
-TxAntNum = 64
-RxAntNum = 128
-ModType = 8
+import numpy as np
 
-startSNR = 10
-endSNR = 25
-stepSNR = 3
-SNR = np.arange(startSNR, endSNR, stepSNR)
 
-target = 2000
+TxAntNum = 8
+RxAntNum = 16
+ModType = 4
 
-def worker(queue, snr, stop_signal):
-    m.detectionInit(TxAntNum, RxAntNum, ModType, snr, False)
-    m.MMSEInit(False)
-    while not stop_signal.is_set():
-        m.execute()
-        errorBits, errorFrames = m.report()
-        queue.put((errorBits, errorFrames))
 
-def run_experiment(snr):
-    num_processes = os.cpu_count()
-    queue = multiprocessing.Queue()
-    stop_signal = multiprocessing.Event()
+startSNR=10
+endSNR=25
+stepSNR=3
+SNR=np.arange(startSNR,endSNR,stepSNR)
 
-    processes = [multiprocessing.Process(target=worker, args=(queue, snr, stop_signal)) for _ in range(num_processes)]
-    for process in processes:
-        process.start()
+target = 20000000
+samplesPreIter=10000
 
-    errorBits = 0
-    errorFrames = 0
-    samples = 0
 
-    try:
-        with tqdm(total=target) as pbar:
-            while errorFrames < target:
-                newErrorBits, newErrorFrames = queue.get()
-                errorBits += newErrorBits
-                errorFrames += newErrorFrames
-                samples += 1
-                pbar.set_postfix(snr=snr, 误码率=errorBits/samples/TxAntNum/ModType, 误帧率=errorFrames/samples, 总错比特=str(errorBits), 总错帧=str(errorFrames), 总样本=str(samples))
-                pbar.update(newErrorFrames)
-    finally:
-        stop_signal.set()
-        for process in processes:
-            process.join()
-
-    return errorBits/samples/TxAntNum/ModType, errorFrames/samples
-
+def worker(queue,samples,snr):
+    while True:
+        errorBits,errorFrames = m.det(TxAntNum,RxAntNum,ModType,snr,samples)
+        queue.put((errorBits,errorFrames))
+ 
 if __name__ == '__main__':
     BERall = []
     FERall = []
+ 
 
     for snr in SNR:
-        ber, fer = run_experiment(snr)
-        BERall.append(ber)
-        FERall.append(fer)
+        num_processes = os.cpu_count() 
+        queue = multiprocessing.Queue()
+        processes = [multiprocessing.Process(target=worker, args=(queue,samplesPreIter,snr)) for i in range(num_processes)]
+        for process in processes:
+            process.start()
+
+        errorBits = 0
+        errorFrames=0
+        samples=0
+
+        with tqdm(total=target) as pbar:
+            while errorFrames < target:
+                (newErrorBits,newErrorFrames) = queue.get()
+        
+                errorBits += newErrorBits
+                errorFrames += newErrorFrames
+                samples += samplesPreIter
+
+                pbar.set_postfix(snr=snr,误码率=errorBits/samples/TxAntNum/ModType,误帧率=errorFrames/samples,总错比特=str(errorBits) ,总错帧=str(errorFrames),总样本=str(samples))
+                
+                pbar.update(newErrorFrames)
+         
+        for process in processes:
+            process.terminate()
+
+        BERall.append(errorBits/samples/TxAntNum/ModType)
+        FERall.append(errorFrames/samples)
